52158cbb62a6cb648f65596a6fbe4dfd2770c29a
Yakir Gagnon <12.yakir@gmail.com>
2021-11-27 16:28:29 +0100

    processed artifact name ref

--- entrypoint2/main.jl
+++ entrypoint2/main.jl
@@ -5,5 +5,5 @@ Pkg.instantiate()
 using Dates, LinearAlgebra, LazyArtifacts
 using CSV, DataFramesMeta
 
-df = DataFrame(CSV.File(artifact"SisyphusCooperationProcessed/processeddata/csvs/data.csv"))
+df = DataFrame(CSV.File(artifact"SisyphusCooperation/processeddata/csvs/data.csv"))
 
